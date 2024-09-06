870d08385ca0b6f30e8a6c05946b9fb816287cb1
Marouane Dbibih <marouane.dbibih@outlook.com>
2024-09-06 02:19:46 +0100

    auth APIs

--- src/main/java/org/fahdpln/backend/employee/EmployeeRepository.java
+++ src/main/java/org/fahdpln/backend/employee/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package org.fahdpln.backend.employee;
 
+import org.fahdpln.backend.departement.Departement;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,5 +18,17 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
             "OR e.job LIKE %:keyword% " +
             "OR e.departement.name LIKE %:keyword%")
     Page<Employee> searchEmployees(@Param("keyword") String keyword, Pageable pageable);
+
+    Page<Employee> findByDepartement(Departement departement, Pageable pageable);
+
+    @Query("SELECT e FROM Employee e " +
+            "JOIN e.user u " +
+            "WHERE (u.username LIKE %:keyword% " +
+            "OR u.name LIKE %:keyword% " +
+            "OR u.email LIKE %:keyword% " +
+            "OR u.phone LIKE %:keyword% " +
+            "OR e.job LIKE %:keyword%) " +
+            "AND e.departement = :departement")
+    Page<Employee> searchEmployeesInDepratement(String keyword, Departement departement, Pageable pageable);
 
 }
